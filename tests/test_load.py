651b8586a119e40e0e6e337fb2990a7e0f6c5a49
Alex Liberzon <alex.liberzon@gmail.com>
2018-07-19 19:45:03 +0100

    fixed the test (see remark on openpiv.tools) and added real checks

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,9 +1,16 @@
-from openpiv import imread
+from openpiv.tools import imread
+
 def test_load():
     """ Unit testing of correct loading of the image """
-    assert imread('../test1/exp1_001_b.bmp')
-    assert imread('../test2/exp1_001_c.bmp')
+    _ = imread('./test1/exp1_001_b.bmp')
+    assert _[0,0] == 8
+    
+    im2 = imread('./test1/exp1_001_c.bmp')
+    assert im2.shape == (369,511)
     
     # at least one failing test 
-    assert imread('../test1/non_existing_image.bmp')
+    try:
+        _ = imread('test1/non_existing_image.bmp')
+    except FileNotFoundError:
+        print('correct testing of a non existing file')   
     
